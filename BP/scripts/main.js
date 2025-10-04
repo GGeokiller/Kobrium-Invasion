4f20518d1b7947cca5ea9155d6194e7d633afca8
EuforiaConR <euforiaconr@gmail.com>
2025-10-04 02:10:40 -0500

    minor changes

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -2,6 +2,9 @@ import { world, system } from "@minecraft/server";
 import "./entities/entityControl";
 import "./items/itemControl";
 import "./blocks/blockControl";
+
+import "./utils/item_components"
+
 system.run(() => {
   world.sendMessage("Hello World");
 });
