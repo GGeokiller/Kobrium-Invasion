b7f67c1f0c0c3ac6fb584c401eb9643a86c6eebc
GGeokiller <geokilleroficial@gmail.com>
2025-10-03 18:32:45 -0600

    main

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,6 +1,6 @@
 import { world, system } from "@minecraft/server"
 import "./entities/entityControl"
-//import "./items/itemControl"
+import "./items/itemControl"
 
 system.run(() => {
     world.sendMessage("Hello World")
