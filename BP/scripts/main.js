4cbeceaaa2bf42909f4e6557b1be22c149964ce1
GGeokiller <geokilleroficial@gmail.com>
2025-10-04 00:33:54 -0600

    block control base

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,7 +1,7 @@
 import { world, system } from "@minecraft/server"
 import "./entities/entityControl"
 import "./items/itemControl"
-
+import "./blocks/blockControl"
 system.run(() => {
     world.sendMessage("Hello World")
 })
