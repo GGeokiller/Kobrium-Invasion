907d7c40bf373c6aaa3741f05502223e601a4c5b
GGeokiller <geokilleroficial@gmail.com>
2025-10-04 04:56:13 -0600

    euforia creo

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,13 +1,9 @@
-import { world, system } from "@minecraft/server";
-import "./entities/entityControl";
-import "./items/itemControl";
-import "./blocks/blockControl";
-
-import "./utils/item_components"
-
+import { world, system } from "@minecraft/server"
+import "./entities/entityControl"
+import "./items/itemControl"
+import "./blocks/blockControl"
 system.run(() => {
-  world.sendMessage("Hello World");
-});
+    world.sendMessage("Hello World")
+})
 
 /// hola de geo
-// hola de euforia
