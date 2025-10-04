b105e286554f9bc840d469fb9872e813a0a1474a
GGeokiller <geokilleroficial@gmail.com>
2025-10-04 15:56:00 -0600

    amplifier particles

--- BP/scripts/entities/kopriumAmplifier.js
+++ BP/scripts/entities/kopriumAmplifier.js
@@ -1,4 +1,11 @@
-import { world, system } from "@minecraft/server";
+import { world, system, Entity } from "@minecraft/server";
+import { geo } from "../utils/geo";
+import { createAreaParticle } from "../utils/particle";
 
-const KOPRIUM_AMPLIFIER_ID = "koprium:amplifier"
+const KOPRIUM_AMPLIFIER_ID = "koprium:koprium_amplifier"
 
+system.runInterval(() => {
+    geo.getEntitiesFromWorld(KOPRIUM_AMPLIFIER_ID).forEach(entity => {
+        createAreaParticle(entity, entity.location, 10)
+    })
+}, 10)
