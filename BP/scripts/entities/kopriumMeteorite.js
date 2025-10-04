b64e842ebfcfb1cd78dcd270d6aaf3991a90b637
EuforiaConR <euforiaconr@gmail.com>
2025-10-03 19:11:36 -0500

    added meteorite explosion logic

--- BP/scripts/entities/kopriumMeteorite.js
+++ BP/scripts/entities/kopriumMeteorite.js
@@ -1,4 +1,4 @@
-import { world, system } from "@minecraft/server"
+import { world, system, BlockPermutation } from "@minecraft/server"
 
 import { Random } from "../utils/random.js"
 
@@ -8,7 +8,7 @@ world.afterEvents.entitySpawn.subscribe(ev => {
     const { entity } = ev
 
     if (entity.typeId == METORITE_ID) {
-        world.sendMessage("Hola Meteorito")
+        console.warn("Hola Meteorito")
 
         const randomDirection = {
             x: (Math.random() - 0.5) * 2,
@@ -16,7 +16,7 @@ world.afterEvents.entitySpawn.subscribe(ev => {
             z: (Math.random() - 0.5) * 2
         }
 
-        const magnitude = 0.2
+        const magnitude = 0.15
         const length = Math.sqrt(randomDirection.x ** 2 + randomDirection.y ** 2 + randomDirection.z ** 2)
         const impulse = {
             x: (randomDirection.x / length) * magnitude,
@@ -34,13 +34,49 @@ world.afterEvents.entitySpawn.subscribe(ev => {
     }
 })
 
+
 world.afterEvents.dataDrivenEntityTrigger.subscribe(ev => {
     const { entity, eventId } = ev
 
     if (entity.typeId == METORITE_ID) {
-        world.sendMessage("explosion")
-        entity.remove()
-        /// meteorite fall logic
+        if (eventId == "kobrium:fall") {
+            console.warn("explosion")
+            const { dimension, location } = entity
+
+            dimension.createExplosion(location, 5, { breaksBlocks: true, causesFire: true, source: entity })
+
+            const radius = 8;
+            system.run(() => {
+                for (let x = -radius; x < radius; x++) {
+                    for (let z = -radius; z < radius; z++) {
+                        if (x * x + z * z <= radius * radius) {
+                            let block = dimension.getBlockBelow({ x: location.x + x, y: location.y, z: location.z + z }, { maxDistance: 10 });
+                            if (!block) { return }
+
+                            if (Random.chance(30)) {
+                                block.setPermutation(BlockPermutation.resolve('minecraft:smooth_basalt'));
+
+                            } else if (Random.chance(10)) {
+                                block.setPermutation(BlockPermutation.resolve('minecraft:nether_gold_ore'));
+
+                            } else if (Random.chance(10)) {
+                                block.setPermutation(BlockPermutation.resolve('minecraft:magma'));
+
+                            } else {
+                                block.setPermutation(BlockPermutation.resolve('minecraft:netherrack'));
+                            }
+                            if (Random.chance(5)) {
+                                block.above().setPermutation(BlockPermutation.resolve('minecraft:sculk_sensor'));
+                            }
+
+                        }
+                    }
+                }
+            });
+
+            entity.remove()
+        }
+
 
     }
 })
