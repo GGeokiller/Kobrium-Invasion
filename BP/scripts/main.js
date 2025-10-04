f56f8c704111e47d26beca986fc2fd4104d0d919
EuforiaConR <euforiaconr@gmail.com>
2025-10-04 02:00:56 -0500

    added moon cow

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -2,8 +2,12 @@ import { world, system } from "@minecraft/server"
 import "./entities/entityControl"
 import "./items/itemControl"
 
+import "./utils/item_components"
+
+
 system.run(() => {
     world.sendMessage("Hello World")
 })
 
 /// hola de geo
+// hola de euforia
