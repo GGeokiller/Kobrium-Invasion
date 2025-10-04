70bda642b7523459db5c1554e0169e7960f49831
GGeokiller <geokilleroficial@gmail.com>
2025-10-04 16:50:24 -0600

    heal particle

--- BP/scripts/entities/kopriumAmplifier.js
+++ BP/scripts/entities/kopriumAmplifier.js
@@ -1,17 +1,30 @@
-import { world, system, Entity, EffectTypes } from "@minecraft/server";
+import { world, system, Entity, EffectTypes, MolangVariableMap } from "@minecraft/server";
 import { geo } from "../utils/geo";
 import { createAreaParticle } from "../utils/particle";
 
 const KOPRIUM_AMPLIFIER_ID = "koprium:koprium_amplifier"
+const KOPRIUM_ENTITIES_COLLISION_BOX = {
+    "koprium:koprium_amplifier": {
+        height: 1.9,
+        width: 0.6
+    },
+    "minecraft:pig" :{
+        width: 0.9,
+        height: 0.9
+    }
+}
 
 system.runInterval(() => {
     geo.getEntitiesFromWorld(KOPRIUM_AMPLIFIER_ID).forEach(entity => {
-            entity.dimension.getEntities({families: ['koprium'], maxDistance: 10, location: entity.location/* , excludeTypes: [KOPRIUM_AMPLIFIER_ID] */}).forEach(kopriumEntity => {
+            entity.dimension.getEntities({families: ['koprium'], maxDistance: 10, location: entity.location, excludeTypes: [KOPRIUM_AMPLIFIER_ID]}).forEach(kopriumEntity => {
                 let health = geo.getHealth(kopriumEntity)
                 let currentValue = health.currentValue
                 let effectiveMax = health.effectiveMax
                 if (currentValue <= effectiveMax && currentValue + 2 <= effectiveMax) {
-                    
+                    let map = new MolangVariableMap()
+                    map.setFloat('variable.height', KOPRIUM_ENTITIES_COLLISION_BOX[kopriumEntity.typeId].height ?? 1)
+                    map.setFloat('variable.width', KOPRIUM_ENTITIES_COLLISION_BOX[kopriumEntity.typeId].width ?? 1)
+                    kopriumEntity.dimension.spawnParticle("koprium:koprium_heal", kopriumEntity.location, map)
                     health.setCurrentValue(currentValue += 2)
                 }
             })
