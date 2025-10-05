12566680156dd6a182a8d14d1da3848a2c9f25e6
GGeokiller <geokilleroficial@gmail.com>
2025-10-05 01:18:09 -0600

    amplifier & rover basic mechanics

--- BP/scripts/entities/kopriumAmplifier.js
+++ BP/scripts/entities/kopriumAmplifier.js
@@ -17,16 +17,19 @@ const KOPRIUM_ENTITIES_COLLISION_BOX = {
 system.runInterval(() => {
     geo.getEntitiesFromWorld(KOPRIUM_AMPLIFIER_ID).forEach(entity => {
         try {
-            let kopriumEntiies = entity.dimension.getEntities({/* families: ['koprium'], */ maxDistance: 10, location: entity.location, excludeTypes: [KOPRIUM_AMPLIFIER_ID]})
+            let kopriumEntiies = entity.dimension.getEntities({families: ['koprium'], maxDistance: 10, location: entity.location, excludeTypes: [KOPRIUM_AMPLIFIER_ID]})
             if (kopriumEntiies.length == 0) return;
             kopriumEntiies.forEach(kopriumEntity => {
+                kopriumEntity.addEffect(EffectTypes.get('strength'), 30, {amplifier: 1})
+                kopriumEntity.addEffect(EffectTypes.get('speed'), 30, {amplifier: 1})
+                kopriumEntity.addEffect(EffectTypes.get('resistance'), 30, {amplifier: 0})
                 let health = geo.getHealth(kopriumEntity)
                 let currentValue = health.currentValue
                 let effectiveMax = health.effectiveMax
                 if (currentValue <= effectiveMax && currentValue + 2 <= effectiveMax) {
                     let map = new MolangVariableMap()
-                    map.setFloat('variable.height', KOPRIUM_ENTITIES_COLLISION_BOX[kopriumEntity.typeId].height ?? 1)
-                    map.setFloat('variable.width', KOPRIUM_ENTITIES_COLLISION_BOX[kopriumEntity.typeId].width ?? 1)
+                    map.setFloat('variable.height', KOPRIUM_ENTITIES_COLLISION_BOX[kopriumEntity?.typeId]?.height ?? 1)
+                    map.setFloat('variable.width', KOPRIUM_ENTITIES_COLLISION_BOX[kopriumEntity?.typeId]?.width ?? 1)
                     kopriumEntity.dimension.spawnParticle("koprium:koprium_heal", kopriumEntity.location, map)
                     health.setCurrentValue(currentValue += 2)
                 }
@@ -34,4 +37,4 @@ system.runInterval(() => {
             createAreaParticle(entity, entity.location, 10)
         } catch{}
     })
-}, 10)
+}, 20)
