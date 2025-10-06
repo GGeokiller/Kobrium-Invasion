d229a1509dd9044b1c78c6130d098b4d5369d065
GGeokiller <geokilleroficial@gmail.com>
2025-10-06 03:53:01 -0600

    fix

--- BP/scripts/entities/koprium/Meteorite.js
+++ BP/scripts/entities/koprium/Meteorite.js
@@ -1,7 +1,7 @@
 import { world, system, BlockPermutation, Entity, BlockType, EffectTypes, DimensionTypes, Block, PlaceJigsawError, EffectType, ScriptEventCommandMessageAfterEvent } from "@minecraft/server"
 
 import { Random } from "../../utils/random.js"
-import { distance } from "../../utils"
+import { distance } from "../../utils/vec3.js"
 //asd
 const METEORITE_ID = "koprium:meteorite"
 const BLOCK_POLL = [
