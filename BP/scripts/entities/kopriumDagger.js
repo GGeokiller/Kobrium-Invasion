7e5c43ef98e97ffd497f86b82f8a26721a16752b
EuforiaConR <euforiaconr@gmail.com>
2025-10-07 04:10:23 -0500

    fixed dagger entity

--- BP/scripts/entities/kopriumDagger.js
+++ BP/scripts/entities/kopriumDagger.js
@@ -1,25 +1,25 @@
-import {world, system} from "@minecraft/server"
+import { world, system } from "@minecraft/server"
 
 const KOPRIUM_DAGGER_ID = "koprium:koprium_dagger"
 const KOPRIUM_DAGGER_PROJECTILE_ID = "koprium:koprium_dagger_projectile"
 
 world.afterEvents.itemUse.subscribe(data => {
-    let {itemStack, source: player} = data
+    let { itemStack, source: player } = data
     if (itemStack?.typeId != KOPRIUM_DAGGER_ID) return;
-    player.playSound("entity.koprium_dagger.throw", {volume: 0.25, pitch: 1})
+    player.playSound("entity.koprium_dagger.throw", { volume: 0.25, pitch: 1 })
 })
 
 world.afterEvents.projectileHitBlock.subscribe(data => {
-    let {projectile} = data
+    let { projectile } = data
     let blockhit = data.getBlockHit()
     if (projectile?.typeId != KOPRIUM_DAGGER_PROJECTILE_ID) return;
-    blockhit.block.dimension.playSound("entity.koprium_dagger.hit", blockhit.block.location, {volume: 3, pitch: 1})
-    projectile.remove()
+    blockhit.block.dimension.playSound("entity.koprium_dagger.hit", blockhit.block.location, { volume: 3, pitch: 1 })
+    //projectile.remove()
 })
 
 world.afterEvents.projectileHitEntity.subscribe(data => {
-    let {projectile} = data
+    let { projectile } = data
     let hitEntity = data.getEntityHit().entity
     if (projectile?.typeId != KOPRIUM_DAGGER_PROJECTILE_ID) return;
-    hitEntity.dimension.playSound("entity.koprium_dagger.hit", hitEntity.location, {volume: 3, pitch: 1})
+    hitEntity.dimension.playSound("entity.koprium_dagger.hit", hitEntity.location, { volume: 3, pitch: 1 })
 })
