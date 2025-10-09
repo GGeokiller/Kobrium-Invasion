bf677335319446f0099fa3066fed4c86af5ffed4
EuforiaConR <euforiaconr@gmail.com>
2025-10-09 11:42:36 -0500

    added generic tags

--- BP/scripts/utils/item_components.js
+++ BP/scripts/utils/item_components.js
@@ -13,4 +13,38 @@ system.beforeEvents.startup.subscribe(({ itemComponentRegistry }) => {
         }
     });
 
+    //maneja la durabilidad de una herramienta al romper un bloque
+    itemComponentRegistry.registerCustomComponent('eu:generic_tool', {
+        onMineBlock(ev) {
+            const { source } = ev
+            const equippable = source.getComponent("minecraft:equippable");
+
+            const mainhand = equippable.getEquipmentSlot("Mainhand");
+            if (!mainhand.hasItem()) return;
+
+            if (source.getGameMode() == "Creative") return;
+
+            const itemStack = mainhand.getItem();
+
+            const durability = itemStack.getComponent("minecraft:durability");
+            if (!durability) return;
+
+            const enchantable = itemStack.getComponent("minecraft:enchantable");
+            const unbreakingLevel = enchantable?.getEnchantment("unbreaking")?.level;
+
+            const damageChance = durability.getDamageChance(unbreakingLevel) / 100;
+
+            if (Math.random() > damageChance) return;
+
+            const shouldBreak = durability.damage == durability.maxDurability;
+
+            if (shouldBreak) {
+                mainhand.setItem(undefined);
+                source.playSound("random.break");
+            } else {
+                durability.damage++;
+                mainhand.setItem(itemStack);
+            }
+        }
+    });
 });
