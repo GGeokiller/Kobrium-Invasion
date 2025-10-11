77992912ea23d1cd4ec6f6996c3e8cd29ad40003
GGeokiller <geokilleroficial@gmail.com>
2025-10-10 19:22:00 -0600

    Koprium Paracore multi texture

--- BP/scripts/entities/koprium/paracore.js
+++ BP/scripts/entities/koprium/paracore.js
@@ -15,7 +15,7 @@ system.runInterval(() => {
         variableMap.setFloat("height", KOPRIUM_ENTITIES_COLLISION_BOX[immortalEntity.typeId].height)
         variableMap.setFloat("width", KOPRIUM_ENTITIES_COLLISION_BOX[immortalEntity.typeId].width)
         immortalEntity.dimension.spawnParticle("koprium:koprium_paracore_effect", immortalEntity.location, variableMap)
-        geoParticles.midDirectionalLineBetween("koprium:koprium_pylon_spawn_line", entity.dimension.id, geo.getEntityCenter(entity), geo.getEntityCenter(immortalEntity), Random.number(0.1, 0.2), 1, 0.2, 0.2, 1, 2, 1);
+        geoParticles.midDirectionalLineBetween("koprium:koprium_pylon_spawn_line", entity.dimension.id, geo.sumVectors(geo.getEntityCenter(entity), {x:0,y:0.9*1.25,z:0}), geo.getEntityCenter(immortalEntity), Random.number(0.1, 0.2), 1, 0.2, 0.2, 1, 2, 1);
     }
 }, 5)
 
